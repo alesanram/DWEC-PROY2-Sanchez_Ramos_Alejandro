964057e73df4954d7ec2692af5c6339de72ea7c7
alesanram <alesanzram@gmail.com>
2022-02-09 17:30:03 +0100

    Terminacion del header

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -4,21 +4,21 @@ import '../index.css';
 
 let Header=()=>{
     return(
-        <Navbar bg="success" expand="lg" variant="dark" fixed="top" >
-            <Container>
-                <Navbar.Brand href="#">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto" fill variant="tabs" defaultActiveKey="#About">
-                    <Nav.Link href="#About" >About Me</Nav.Link>
-                    <Nav.Link href="#Jobs">Mis Proyectos</Nav.Link>
-                    <Nav.Link href="#Academics">Mis Titulos</Nav.Link>
-                    <Nav.Link href="#Utylitis">Utilidades</Nav.Link>
-                    <Nav.Link href="#Recuperacion">Recuperacion UD1</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+            <Navbar className="header"  bg="success" expand="lg" variant="dark" fixed="top" >
+                <Container>
+                    <Navbar.Brand href="#">Alejandro</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto" fill variant="tabs" defaultActiveKey="#About">
+                        <Nav.Link href="#About" >About Me</Nav.Link>
+                        <Nav.Link href="#Jobs">Mis Proyectos</Nav.Link>
+                        <Nav.Link href="#Academics">Mis Titulos</Nav.Link>
+                        <Nav.Link href="#Utylitis">Utilidades</Nav.Link>
+                        <Nav.Link href="#Recuperacion">Recuperacion UD1</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
     )
 }
 
