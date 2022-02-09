9fe88e83bd444285343d26c5860d00227b36624e
alesanram <alesanzram@gmail.com>
2022-02-09 17:30:32 +0100

    Terminacion del about me sin texto y css incompleto

--- src/componentes/about_me.js
+++ src/componentes/about_me.js
@@ -1,13 +1,32 @@
 import React from "react";
-import bootstrap from "bootstrap";
+import {Carousel} from 'react-bootstrap';
+import img from  "../img/fondo_about.png"
 
 let AboutMe=()=>{
     return(
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nisi leo, maximus sit amet consequat id, mollis eget velit. Mauris ullamcorper lacus erat, in posuere velit posuere eu. Etiam urna magna, sodales nec eleifend pharetra, condimentum vel velit. Fusce fermentum ultrices turpis, sit amet luctus mauris tristique id. Maecenas commodo feugiat tristique. Proin suscipit, risus et tincidunt efficitur, elit nisi lobortis augue, ut ultricies risus mi et ligula. Curabitur ultrices efficitur laoreet. Maecenas facilisis lacinia leo nec congue. Vivamus vitae efficitur nunc. Vivamus scelerisque purus eu tellus mattis convallis. Cras laoreet, augue vel iaculis mattis, mauris lacus rhoncus massa, finibus tristique nunc urna non nibh. Donec a lorem eu ligula congue viverra eget sit amet purus. Sed pulvinar risus eu urna commodo, vel gravida nisl sollicitudin. Nunc sit amet nulla rutrum, egestas nisi at, venenatis erat. Nullam tincidunt luctus porttitor. Vestibulum fringilla viverra lectus, sit amet tempor nunc feugiat vitae.
-
-Suspendisse potenti. Nulla et elit sed dui luctus sodales eu nec augue. Donec fermentum elit quis bibendum imperdiet. Mauris semper urna at ex aliquet, eget sodales magna laoreet. Quisque nisl dolor, porttitor id lacinia ac, facilisis sit amet sapien. Integer varius, est eget euismod semper, mi dolor rutrum diam, ultrices tincidunt odio lacus tincidunt velit. Nunc a velit ut metus blandit finibus. Pellentesque eu auctor magna, quis laoreet sapien.
-        </p>
+        <Carousel className="about">
+            <Carousel.Item>
+                <img className="about__image" src={img} alt="Foto de Peter Olexa en Pexels"/>
+                <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className="about__image" src={img} alt="Foto de Peter Olexa en Pexels"/>
+                <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className="about__image" src={img} alt="Foto de Peter Olexa en Pexels"/>
+                <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     )
 }
 export default AboutMe;
