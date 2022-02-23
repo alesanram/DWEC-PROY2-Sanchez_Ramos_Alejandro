d7542271cfd74932fb863dad06c731698593ba0b
alesanram <alesanzram@gmail.com>
2022-02-23 14:58:26 +0100

    crear componente de recuperacion con eventos de teclado

---
+++ src/componentes/ud1_recuperacion.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+let Recuperacion=()=>{
+    document.addEventListener('keydown', (event) => {
+        let articulo=document.getElementById("articulo");
+        if (event.key === "r" || event.key === "R") {
+            articulo.classList.remove("oculto");
+        }
+        if (event.key === "g" || event.key === "G") {
+            window.scrollTo(0, 0);
+        }
+        if ((event.key === "n" || event.key === "N") && !articulo.classList.contains("oculto") ) {
+            articulo.classList.add("oculto");
+        }
+    }, false);
+    return(
+        <div id="Recuperacion">
+            <h3>Presiona R para ver el articulo o G para volver al juego</h3>
+            <div id="articulo" className="oculto">
+                <h3>Pulse N para cerrar el articulo</h3>
+            </div>
+        </div>
+    )
+}
+export default Recuperacion;
