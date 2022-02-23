546d96bf447e6fcc1908fefc37f3ed1d80e2a1c5
alesanram <alesanzram@gmail.com>
2022-02-23 23:36:00 +0100

    comentar el academic y añdir otros titulos

--- src/componentes/academics.js
+++ src/componentes/academics.js
@@ -2,6 +2,7 @@ import {React} from "react";
 import {Accordion} from 'react-bootstrap';
 let Academics=()=>{
     return(
+        //Acrodeon generado con boostrap con mis titulos academicos(como si hubiera terminado el grado)
         <Accordion  bg="success" className="acordeon" id="Academics">
             <Accordion.Item eventKey="0">
                 <Accordion.Header>Mis Titulos</Accordion.Header>
@@ -9,6 +10,12 @@ let Academics=()=>{
                     <ul>
                         <li>
                             <p>Tecnico Superior en Desarrollo de aplicaciones web</p>
+                        </li>
+                        <li>
+                            <p>Curos básico de prevención de riesgos laborales</p>
+                        </li>
+                        <li>
+                            <p>B1- Ingles</p>
                         </li>
                     </ul>
                 </Accordion.Body>
