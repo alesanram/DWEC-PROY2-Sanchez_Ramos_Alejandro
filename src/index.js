4abd82dfabd757225c7c62821f23a6224f744440
alesanram <alesanzram@gmail.com>
2022-02-23 23:37:11 +0100

    modificar el index.js al cambio de nombre de jobs

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ import ReactDOM from 'react-dom';
 import Header from "./componentes/header"
 import Footer from './componentes/footer';
 import About from './componentes/about';
-import Jobs from './componentes/jobs';
+import Porfolio from './componentes/porfolio';
 import Utility from './componentes/utility';
 import Academics from './componentes/academics';
 import Recuperacion from './componentes/ud1_recuperacion';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
+  //Componente donde se monta la pagina con el resto de los componentes esta en un div porque a react no le gustan los componentes sueltos
   <div className='contenedor'>
     <Header/>
     <Utility/>  
     <About/>
     <Academics/>
-    <Jobs/>
+    <Porfolio/>
     <Recuperacion/>
     <Footer/>
   </div>
