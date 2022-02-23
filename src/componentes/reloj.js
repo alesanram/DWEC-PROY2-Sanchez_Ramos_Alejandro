526fda50ed6a2e82bd40c21a0898c514c46c955d
alesanram <alesanzram@gmail.com>
2022-02-23 17:11:57 +0100

    Actualizar el reloj para que se autalize la hora

--- src/componentes/reloj.js
+++ src/componentes/reloj.js
@@ -1,10 +1,17 @@
-import React from "react";
+import {React,useEffect,useState} from "react";
 import moment from 'moment';
 import 'moment/locale/es';
 
 let Reloj=()=>{
+    const [time, setTime] = useState(moment().locale('es').format('llll'));
+    useEffect(() => {
+        const interval = setInterval(
+            () => setTime(time => moment().locale('es').format('llll')), 1000
+        );
+        return () => clearInterval(interval);
+    });
     return(
-        <h5>{moment().locale('es').format('llll')}</h5>
+        <h5>{time}</h5>
     );
 }
 
