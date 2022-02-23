66726fc9aefc63c92c53d4181acd6e7c83747540
alesanram <alesanzram@gmail.com>
2022-02-23 23:36:18 +0100

    añadir commentarios y daotos al footer

--- src/componentes/footer.js
+++ src/componentes/footer.js
@@ -1,13 +1,24 @@
-import react from "react";
+import React from "react";
 import {Container,Navbar} from 'react-bootstrap'
 import '../index.css';
 
 
 let Footer=()=>{
+    //Footer creado con el nav de boostrap para que tenga el mismo aspcto que el header contiene datos de contacto
     return(
         <Navbar bg="success" expand="lg" variant="dark" >
             <Container>
-                
+                <ul className="footer">
+                    <li>
+                        <p>Github: <a href="https://github.com/alesanram">alesanram</a></p>
+                    </li>
+                    <li>
+                        <p>Correo: alejsanzram@gmail.com</p>
+                    </li>
+                    <li>
+                        <p>telefono: 684269375</p>
+                    </li>
+                </ul>
             </Container>
         </Navbar>
     )
