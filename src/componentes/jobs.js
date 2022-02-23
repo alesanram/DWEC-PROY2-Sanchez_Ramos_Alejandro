6dc9466319b35749fae0d8ef7be4d543a919d755
alesanram <alesanzram@gmail.com>
2022-02-23 15:03:43 +0100

    añadir lo necesario para publicarlo en github

--- src/componentes/jobs.js
+++ src/componentes/jobs.js
@@ -44,6 +44,20 @@ let Jobs=()=>{
                 <small className="text-muted">Last updated 3 mins ago</small>
                 </Card.Footer>
             </Card>
+            <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                <Card.Title>Porfolio</Card.Title>
+                <Card.Text>
+                    This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This card has even longer content than the first to
+                    show that equal height action.
+                </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+            </Card>
         </CardGroup>
     )
 }
